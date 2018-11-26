d7c26dabeb1d34e14460bf5befca8564c1b38655
Kevin Huang <yellow.khaos@gmail.com>
2018-11-26 00:59:30 +0000

    automatically backed up by learn

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -2,3 +2,5 @@ function hideWhenClicked() {
   let ele = document.getElementById('hide_this')
   ele.style.display = "none"
 }
+
+document.getElementById('hide_this').addEventListener('click', hideWhenClicked())
